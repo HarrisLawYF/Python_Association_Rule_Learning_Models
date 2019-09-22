e4a8e99f88f0c12f9ba38fd97a976350b048ffd5
Harris Law <lawharris92@gmail.com>
2019-09-22 19:19:37 +1000

    associate rule learning v1

---
+++ Apriori/apriori.py
@@ -0,0 +1,23 @@
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+# Training Apriori on the dataset
+from apyori import apriori
+# min_length, the minimum product you want in a basket
+# min_support: the minimum purchase frequency of a product each day
+# min_confidence: the accurency of the rule being correct in all transactions
+# min_lift: threshold for accepting the rule
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualising the results
+results = list(rules)
